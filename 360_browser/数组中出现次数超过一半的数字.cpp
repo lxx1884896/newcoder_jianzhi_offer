b200558b46dd15ab2296dfafc87a62f1398c936a
lxx1884896 <1523631706@qq.com>
2019-06-28 17:19:12 +0800

    更新了  数组中超过一半的数  那道题

--- 360_browser/数组中出现次数超过一半的数字.cpp
+++ 360_browser/数组中出现次数超过一半的数字.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    //二刷补充：可以用partition找中位数 o(n)，还有 剑指上：如果下一个相同则加一，不同则减，
+    //二刷补充：可以用partition找中位数 o(n)  （该方法试验后不太行？），还有 剑指上：如果下一个相同则加一，不同则减，在下面贴出
     int MoreThanHalfNum_Solution(vector<int> numbers) {
         int len=numbers.size();
         if(len==0) return 0;
@@ -22,4 +22,35 @@ public:
         return 0;
         
     }
+};
+
+
+//leetcode   AC代码
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        int len=nums.size();
+        int result=nums[0];
+        int times=1;
+        for(int i=1;i<len;i++){
+            
+            if(times==0){
+                
+                result=nums[i];
+                times=1;
+            }
+            else if(nums[i]==result){
+                
+                times++;
+            }
+            
+            else{
+                
+                times--;
+            }
+        }
+        
+        return result;
+    }       
 };
