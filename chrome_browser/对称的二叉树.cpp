70a437f9ef5f5ae82ab455f50b0a6be1f07350f1
lxx1884896 <1523631706@qq.com>
2022-02-19 20:15:23 +0800

    add 非递归的层序遍历解法

--- chrome_browser/对称的二叉树.cpp
+++ chrome_browser/对称的二叉树.cpp
@@ -28,3 +28,42 @@ public:
     }
 
 };
+
+//对称的二叉树非递归
+
+/*
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
+};
+*/
+class Solution {
+public:
+    bool isSymmetrical(TreeNode* pRoot) {
+        if (!pRoot)
+            return true;
+        queue<TreeNode*> q; // 定义队列
+        q.push(pRoot); q.push(pRoot); // 根结点入队两次
+        while (!q.empty()) {
+            TreeNode* n1 = q.front();
+            q.pop();
+            TreeNode* n2 = q.front();
+            q.pop();
+           
+            if (!n1 && !n2) // 判断是否全部为空
+                continue;
+            if(!n1 || !n2) return false;
+            if (n1->val != n2->val) // 判断值是否相等
+                return false;
+            // 将四个孩子入队列
+            q.push(n1->left); q.push(n2->right);
+            q.push(n1->right); q.push(n2->left);
+        }
+        return true;
+    }
+
+};
